73dfff6f7568706156dad20dd6a410c1ba72ebc7
Prince Bharti <bhartiprince829@gamil.com>
2026-06-10 20:07:58 +0530

    Added Sort Color Solution

---
+++ Day2/Sort_Colors.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        return sort(nums.begin(), nums.end());
+    }
+};
